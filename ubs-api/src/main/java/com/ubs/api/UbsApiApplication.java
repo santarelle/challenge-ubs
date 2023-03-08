a721f92b2e50a1db5bc270ed12209183e954cd07
Marco Jardim <marco.jardim@gft.com>
2023-03-07 21:24:18 -0300

    feat: add initial project

---
+++ ubs-api/src/main/java/com/ubs/api/UbsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ubs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbsApiApplication.class, args);
+	}
+
+}
